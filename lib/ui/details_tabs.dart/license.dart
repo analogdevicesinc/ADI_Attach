90d68ba85f3f67167c316882370841d1aae89f0c
Ahmet Polat <Ahmet.Polat@maximintegrated.com>
2023-01-11 11:22:40 +0300

    License page updated to show license.txt

--- lib/ui/details_tabs.dart/license.dart
+++ lib/ui/details_tabs.dart/license.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class LicenseView extends StatelessWidget {
+class LicenseView extends StatefulWidget {
   const LicenseView({Key? key}) : super(key: key);
+
+  @override
+  State<LicenseView> createState() => _LicenseViewState();
+}
+
+class _LicenseViewState extends State<LicenseView> {
+  String? data;
+
+  void _loadData() async {
+    final _loadedData = await rootBundle.loadString('assets/license.txt');
+    setState(() {
+      data = _loadedData;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('License'),
-      ),
-      body: const Center(child: Text('License Page')),
-    );
+        appBar: AppBar(
+          title: const Text('License'),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(data ?? ''),
+          ),
+        ));
   }
 }
