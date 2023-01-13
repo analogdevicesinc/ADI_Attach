c08f185e84c420982221f957d57a7ba360aa3366
Ahmet Polat <44245503+polat-ahmet@users.noreply.github.com>
2023-01-13 10:04:30 +0300

    Add a button to clear logs (#3)
    * added clearLog func to LogState Provider Class
    * ClearLogButton added to logs section

--- lib/global/logger.dart
+++ lib/global/logger.dart
@@ -9,6 +9,11 @@ class LogState extends ChangeNotifier {
     notifyListeners();
   }
 
+  void clearLog() {
+    _appLog = '';
+    notifyListeners();
+  }
+
   String get appLog => _appLog;
 }
 
@@ -24,18 +29,57 @@ class _LogTabState extends State<LogTab> {
 
   @override
   Widget build(BuildContext context) {
-    return SelectableRegion(
-      selectionControls: materialTextSelectionControls,
-      focusNode: FocusNode(),
-      child: Scrollbar(
-        thumbVisibility: true,
-        trackVisibility: true,
-        thickness: 5,
-        radius: const Radius.circular(2.5),
-        controller: _logScrollController,
-        child: SingleChildScrollView(
-          controller: _logScrollController,
-          child: CPText(Provider.of<LogState>(context).appLog),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(thickness: 1.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'LOGS',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                SizedBox(),
+                ElevatedButton(
+                  onPressed: () {
+                    Provider.of<LogState>(context, listen: false).clearLog();
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Clear Logs'),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.delete,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SelectableRegion(
+              selectionControls: materialTextSelectionControls,
+              focusNode: FocusNode(),
+              child: Scrollbar(
+                thumbVisibility: true,
+                trackVisibility: true,
+                thickness: 5,
+                radius: const Radius.circular(2.5),
+                controller: _logScrollController,
+                child: SingleChildScrollView(
+                  controller: _logScrollController,
+                  child: CPText(Provider.of<LogState>(context).appLog),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
